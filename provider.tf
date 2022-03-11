5ebb3292f347e9b0afe37624488164fba09bb032
vipin104766 <vipin@selfcode.in>
2022-03-11 12:22:26 +0530

    added default tags

--- provider.tf
+++ provider.tf
@@ -12,4 +12,11 @@ provider "aws" {
   region  = var.region
   access_key = var.access_key
   secret_key = var.secret_key
+  default_tags {
+   tags = {
+     Environment = "Test"
+     Owner       = "TFProviders"
+     Project     = "Test"
+   }
+ }
 }
