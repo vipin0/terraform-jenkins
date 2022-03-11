491d020adce175213e69f83b01d6c721426f77b7
vipin0 <vipin6673@gmail.com>
2022-03-11 15:05:53 +0530

    updated

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "instance_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.instance_ssh_cidrs
+    cidr_blocks = ["0.0.0.0/0"]
   }
   # outbound rules
   egress {
