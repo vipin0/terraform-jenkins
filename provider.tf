fe6d52f3c5111c9b3f34721c39e0779f3a81cf03
vipin0 <vipin6673@gmail.com>
2022-03-11 14:11:51 +0530

    added s3 remote state backend

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket = "vipin-terraform-state"
+    key    = "tf-jenkins/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "vipin-tf-state-lock"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
