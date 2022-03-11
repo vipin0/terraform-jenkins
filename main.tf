8333e9829f8cee41de6f7eff731b1da55062605c
vipin0 <vipin6673@gmail.com>
2022-03-11 15:23:38 +0530

    added userdate

--- main.tf
+++ main.tf
@@ -40,6 +40,8 @@ resource "aws_instance" "app_server" {
   key_name                    = var.key_name
   vpc_security_group_ids      = [aws_security_group.instance_sg.id]
   associate_public_ip_address = var.associate_public_ip_address
+  
+  user_data = "${file("user-data.sh")}"
 
   tags = {
     Name = "webserver"
