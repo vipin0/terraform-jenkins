25a1e8635f2fef2eeaec7c962fa6c39db7b62a91
vipin0 <vipin6673@gmail.com>
2022-03-11 15:03:12 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -18,9 +18,8 @@ terraform {
 provider "aws" {
   # Configuration options
   region     = var.region
-  # access_key = var.access_key
-  # secret_key = var.secret_key
-  profile = "vipin-personal"
+  access_key = var.access_key
+  secret_key = var.secret_key
   default_tags {
     tags = var.default_tags
   }
